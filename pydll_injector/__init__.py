ad4ca417cee22186a3d7d85a591622bf811437da
jokelbaf <60827680+JokelBaf@users.noreply.github.com>
2025-07-30 03:57:10 +0300

    chore(ver): Bump version

--- pydll_injector/__init__.py
+++ pydll_injector/__init__.py
@@ -2,7 +2,7 @@
 from .process import spawn_process, is_process_running, terminate_process
 from .models import Environment, Launcher, Context
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = [
     "spawn_process",
